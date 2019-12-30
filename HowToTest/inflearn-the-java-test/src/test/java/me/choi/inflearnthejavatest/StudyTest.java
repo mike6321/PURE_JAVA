a966c8fd38b4c55858668f793c57f159908cb7b1
choijunwoo <mike6321@naver.com>
2019-12-30 21:18:09 +0900

    테스트 반복(1)

--- HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/StudyTest.java
+++ HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/StudyTest.java
@@ -1,6 +1,8 @@
 package me.choi.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 import java.util.function.Supplier;
@@ -23,6 +25,20 @@ class StudyTest {
     //@Tag("slow")
     public void tesing_intelij() {
         System.out.println("test!");
+    }
+
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition} / {totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("repeate test :: "+ repetitionInfo.getCurrentRepetition() + "/"+
+                repetitionInfo.getTotalRepetitions());
+    }
+
+    @DisplayName("스터디 만들기 2")
+    @ParameterizedTest(name = "{index} {displayName} message = {0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있습니다."})
+    void ParameterizedTest(String message) {
+        System.out.println(message);
     }
 
     @Test
