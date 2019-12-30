acf62cc2c76e0cc022afe6c8199c009fc53bbc89
choijunwoo <mike6321@naver.com>
2019-12-30 22:52:15 +0900

    ArgumentsAccessor의 사용

--- HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/StudyTest.java
+++ HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/StudyTest.java
@@ -2,6 +2,7 @@ package me.choi.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
@@ -43,13 +44,10 @@ class StudyTest {
     @DisplayName("스터디 만들기 2")
     @ParameterizedTest(name = "{index} {displayName} message = {0}")
     @CsvSource({"10, '자바 스터디'", "20, 스프"})
-    void ParameterizedTest(Integer limit, String name) {
-        Study study = new Study(limit, name);
+    void ParameterizedTest(ArgumentsAccessor argumentsAccessor) {
+        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
         System.out.println(study);
     }
-
-
-
 
     static class StudyConverter extends SimpleArgumentConverter {
 
