395d57e079fe9046f3e5a5f32cda3cc09d25382d
choijunwoo <mike6321@naver.com>
2019-12-30 23:47:21 +0900

    test instance의 사용

--- HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/StudyTest.java
+++ HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/StudyTest.java
@@ -20,71 +20,34 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class StudyTest {
+
+    int val = 1;
 
     @FastTest
     @DisplayName("주누의 개인공부 시간 fast")
-        //@Tag("fast")
     void create_new_study() {
-        //assumeTrue("LOCAL".equals(System.getenv("TEST_ENV")));
-        Study study = new Study(10);
+        System.out.println(this);
+        System.out.println(val++);
     }
     @SlowTest
     @DisplayName("주누의 개인공부 시간 slow")
-    //@Tag("slow")
-    public void tesing_intelij() {
-        System.out.println("test!");
+    public void create_new_study2() {
+        System.out.println(this);
+        System.out.println(val++);
     }
 
-    @DisplayName("스터디 만들기")
-    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition} / {totalRepetitions}")
-    void repeatTest(RepetitionInfo repetitionInfo) {
-        System.out.println("repeate test :: "+ repetitionInfo.getCurrentRepetition() + "/"+
-                repetitionInfo.getTotalRepetitions());
-    }
-
-
-
-    @DisplayName("스터디 만들기 2")
-    @ParameterizedTest(name = "{index} {displayName} message = {0}")
-    @CsvSource({"10, '자바 스터디'", "20, 스프"})
-    void ParameterizedTest(@AggregateWith(StudyAggregator.class)Study study) {
-    //        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
-        System.out.println(study);
-    }
-
-    static class StudyAggregator implements ArgumentsAggregator {
-
-        @Override
-        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
-            return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
-        }
-    }
-
-    static class StudyConverter extends SimpleArgumentConverter {
-
-        @Override
-        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
-            assertEquals(Study.class, targetType, "Can only convert to Study");
-            return Integer.parseInt(source.toString());
-        }
-    }
-
-    @Test
-        //@Disabled
-    void create2() {
-        System.out.println("create2");
-    }
 
 
 
     @BeforeAll
-    static void beforeAll() {
+    void beforeAll() {
         System.out.println("beforeAll");
     }
 
     @AfterAll
-    static void afterAll() {
+    void afterAll() {
         System.out.println("AfterAll");
     }
 
