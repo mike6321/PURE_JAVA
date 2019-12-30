5b80d7f465f312c33612491f6bb5830356c362c5
choijunwoo <mike6321@naver.com>
2019-12-30 23:13:49 +0900

    aggregator의 활용

--- HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/StudyTest.java
+++ HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/StudyTest.java
@@ -1,8 +1,12 @@
 package me.choi.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
 import org.junit.jupiter.params.converter.ArgumentConversionException;
 import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.converter.SimpleArgumentConverter;
@@ -44,9 +48,17 @@ class StudyTest {
     @DisplayName("스터디 만들기 2")
     @ParameterizedTest(name = "{index} {displayName} message = {0}")
     @CsvSource({"10, '자바 스터디'", "20, 스프"})
-    void ParameterizedTest(ArgumentsAccessor argumentsAccessor) {
-        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+    void ParameterizedTest(@AggregateWith(StudyAggregator.class)Study study) {
+    //        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
         System.out.println(study);
+    }
+
+    static class StudyAggregator implements ArgumentsAggregator {
+
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
+            return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
+        }
     }
 
     static class StudyConverter extends SimpleArgumentConverter {
