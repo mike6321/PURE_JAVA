bb6f01a400f292ca0743fcc6305f9d30361d8ab9
choijunwoo <mike6321@naver.com>
2019-12-30 19:37:01 +0900

    junit5 tagging and filtering

--- HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/StudyTest.java
+++ HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/StudyTest.java
@@ -6,45 +6,32 @@ import java.time.Duration;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
-    @DisplayName("주누의 개인공부 시간")
+    @DisplayName("주누의 개인공부 시간 fast")
+    @Tag("fast")
     void create_new_study() {
-        Study study = new Study(-10);
-
-//        assertNotNull(study);
-//        //assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값은 DRAFT여야 한다.");
-//        assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값은 DRAFT여야 한다.");
-//        assertTrue(study.getLimit() > 0, "스터디 최대 참석인원은 0명 보다 커야 합니다.");
-
-//        assertAll(
-//                () -> assertNotNull(study),
-//                () -> assertEquals(StudyStatus.DRAFT, study.getStatus(), () -> "스터디를 처음 만들면 상태값은 DRAFT여야 한다."),
-//                () ->assertTrue(study.getLimit() > 0, "스터디 최대 참석인원은 0명 보다 커야 합니다.")
-//        );
-//
-//        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-50));
-//        String message = exception.getMessage();
-//        assertEquals(message, "학생 수가 너무 작아요");
-
-        assertTimeoutPreemptively(Duration.ofMillis(100), () ->{
-            new Study(10);
-            Thread.sleep(30000);
-        });
+        //assumeTrue("LOCAL".equals(System.getenv("TEST_ENV")));
+        Study study = new Study(10);
     }
     @Test
-    @Disabled
+    @DisplayName("주누의 개인공부 시간 slow")
+    @Tag("slow")
+    public void tesing_intelij() {
+        System.out.println("test!");
+    }
+
+    @Test
+    //@Disabled
     void create2() {
         System.out.println("create2");
     }
 
-    @Test
-    public void tesing_intelij() {
-        System.out.println("test!");
-    }
+
 
     @BeforeAll
     static void beforeAll() {
