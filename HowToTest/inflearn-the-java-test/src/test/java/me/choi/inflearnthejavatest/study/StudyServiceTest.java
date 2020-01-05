964d0bb8547eb290a548e6838eaf9254580d2020
choijunwoo <mike6321@naver.com>
2020-01-05 15:50:39 +0900

    Mock 객체를 하나의 테스트에서 이용 (파라미터에 Mock 설정)

--- HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/study/StudyServiceTest.java
+++ HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/study/StudyServiceTest.java
@@ -1,38 +1,20 @@
 package me.choi.inflearnthejavatest.study;
 
-import me.choi.inflearnthejavatest.Study;
-import me.choi.inflearnthejavatest.domain.Member;
-import me.choi.inflearnthejavatest.member.MemberNotFoundException;
 import me.choi.inflearnthejavatest.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
-    @Mock
-    MemberService memberService;
-
-    @Mock
-    StudyRepository studyRepository;
 
     @Test
-    void createStudyService() {
-
-        StudyService studyService = new StudyService(memberService,studyRepository);
+    void createStudyService(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
+        StudyService studyService = new StudyService(memberService, studyRepository);
 
         assertNotNull(studyRepository);
     }
