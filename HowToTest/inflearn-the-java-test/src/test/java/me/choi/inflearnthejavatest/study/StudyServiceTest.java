33b6f69cab887f2aa47ecf56b78954a84aab815b
choijunwoo <mike6321@naver.com>
2020-01-05 16:02:16 +0900

    모든 Mock 객체의 행동

--- HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/study/StudyServiceTest.java
+++ HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/study/StudyServiceTest.java
@@ -1,12 +1,16 @@
 package me.choi.inflearnthejavatest.study;
 
+import me.choi.inflearnthejavatest.domain.Member;
 import me.choi.inflearnthejavatest.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -14,6 +18,10 @@ class StudyServiceTest {
 
     @Test
     void createStudyService(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
+        Optional<Member> optional = memberService.findById(1L);
+        assertNull(optional);
+
+        memberService.validate(1L);
         StudyService studyService = new StudyService(memberService, studyRepository);
 
         assertNotNull(studyRepository);
