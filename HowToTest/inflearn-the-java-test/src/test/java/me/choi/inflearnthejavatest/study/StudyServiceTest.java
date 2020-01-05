e510ddd4eedc23c45a84a36b5d8f6eb0009a705f
choijunwoo <mike6321@naver.com>
2020-01-05 15:29:27 +0900

    mockito 사용이유

--- HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/study/StudyServiceTest.java
+++ HowToTest/inflearn-the-java-test/src/test/java/me/choi/inflearnthejavatest/study/StudyServiceTest.java
@@ -1,7 +1,155 @@
 package me.choi.inflearnthejavatest.study;
 
+import me.choi.inflearnthejavatest.Study;
+import me.choi.inflearnthejavatest.domain.Member;
+import me.choi.inflearnthejavatest.member.MemberNotFoundException;
+import me.choi.inflearnthejavatest.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudyServiceTest {
+
+    @Test
+    void createStudyService() {
+        MemberService memberService = new MemberService() {
+            @Override
+            public Optional<Member> findById(Long memberId) throws MemberNotFoundException {
+                return Optional.empty();
+            }
+        };
+
+        StudyRepository studyRepository = new StudyRepository() {
+            @Override
+            public List<Study> findAll() {
+                return null;
+            }
+
+            @Override
+            public List<Study> findAll(Sort sort) {
+                return null;
+            }
+
+            @Override
+            public List<Study> findAllById(Iterable<Long> iterable) {
+                return null;
+            }
+
+            @Override
+            public <S extends Study> List<S> saveAll(Iterable<S> iterable) {
+                return null;
+            }
+
+            @Override
+            public void flush() {
+
+            }
+
+            @Override
+            public <S extends Study> S saveAndFlush(S s) {
+                return null;
+            }
+
+            @Override
+            public void deleteInBatch(Iterable<Study> iterable) {
+
+            }
+
+            @Override
+            public void deleteAllInBatch() {
+
+            }
+
+            @Override
+            public Study getOne(Long aLong) {
+                return null;
+            }
+
+            @Override
+            public <S extends Study> List<S> findAll(Example<S> example) {
+                return null;
+            }
+
+            @Override
+            public <S extends Study> List<S> findAll(Example<S> example, Sort sort) {
+                return null;
+            }
+
+            @Override
+            public Page<Study> findAll(Pageable pageable) {
+                return null;
+            }
+
+            @Override
+            public <S extends Study> S save(S s) {
+                return null;
+            }
+
+            @Override
+            public Optional<Study> findById(Long aLong) {
+                return Optional.empty();
+            }
+
+            @Override
+            public boolean existsById(Long aLong) {
+                return false;
+            }
+
+            @Override
+            public long count() {
+                return 0;
+            }
+
+            @Override
+            public void deleteById(Long aLong) {
+
+            }
+
+            @Override
+            public void delete(Study study) {
+
+            }
+
+            @Override
+            public void deleteAll(Iterable<? extends Study> iterable) {
+
+            }
+
+            @Override
+            public void deleteAll() {
+
+            }
+
+            @Override
+            public <S extends Study> Optional<S> findOne(Example<S> example) {
+                return Optional.empty();
+            }
+
+            @Override
+            public <S extends Study> Page<S> findAll(Example<S> example, Pageable pageable) {
+                return null;
+            }
+
+            @Override
+            public <S extends Study> long count(Example<S> example) {
+                return 0;
+            }
+
+            @Override
+            public <S extends Study> boolean exists(Example<S> example) {
+                return false;
+            }
+        };
+        StudyService studyService = new StudyService(memberService,studyRepository);
+
+        assertNotNull(studyRepository);
+    }
 
 }
