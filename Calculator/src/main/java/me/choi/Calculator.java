d1bb66e1e174aed971b12a38b960dfcc1cd31066
choijunwoo <mike6321@naver.com>
2020-01-14 23:13:19 +0900

    계산기 초기 source 만들기

---
+++ Calculator/src/main/java/me/choi/Calculator.java
@@ -0,0 +1,24 @@
+package me.choi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Calculator {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Start the Calculator Program...");
+        Thread.sleep(1000);
+
+        System.out.println("수식을 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+        String calcStr = scanner.next();
+
+        /*예외처리*/
+
+
+    }
+
+
+}
