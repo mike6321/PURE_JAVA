0cd69b3375340cbc0847a7c504d660f0642ca522
choijunwoo <mike6321@naver.com>
2020-01-19 23:22:23 +0900

    thread의 호출스택-생성시점에 강제 종료

--- Thread/src/main/java/me/choi/ThreadEx.java
+++ Thread/src/main/java/me/choi/ThreadEx.java
@@ -4,19 +4,19 @@ public class ThreadEx {
     public static void main(String[] args) {
         ThreadEx1_1 th1 = new ThreadEx1_1();
         th1.start();
-
-        Runnable r = new ThreadEx1_1();
-        Thread th2 = new Thread(r);
-        th2.start();
-//        th2.start();
     }
 }
 
 class ThreadEx1_1 extends Thread{
     @Override
     public void run() {
-        for (int i=0;i<5;i++){
-            System.out.println(getName());
+        ThrowException();
+    }
+    public void ThrowException () {
+        try {
+            throw  new Exception();
+        }catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
