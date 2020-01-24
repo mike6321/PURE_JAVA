9348c49cc9c5e7f048c4ca32f1b0164e93db6783
choijunwoo <mike6321@naver.com>
2020-01-24 23:34:56 +0900

    deamonThread의 활용

--- Thread/src/main/java/me/choi/DeamonThreadEx.java
+++ Thread/src/main/java/me/choi/DeamonThreadEx.java
@@ -5,30 +5,31 @@ public class DeamonThreadEx implements Runnable{
 
     public static void main(String[] args) {
         Thread th = new Thread(new DeamonThreadEx());
-        th.setDaemon(true);
+        //th.setDaemon(true);
         th.start();
 
         for (int i=0;i<10;i++) {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
-                System.out.println(i);
+            }
 
-                if(i==5) {
-                    autoSave = true;
-                }
+            System.out.println(i);
+            if(i==5) {
+                autoSave = true;
             }
         }
+        System.out.println("프로그램을 종료합니다...");
     }
     @Override
     public void run() {
         while (true) {
             try {
-                Thread.sleep(3*1000);
+                Thread.sleep(1*1000);
             } catch (InterruptedException e) {
-                if (autoSave) {
-                    autoSave();
-                }
+            }
+            if (autoSave) {
+                autoSave();
             }
         }
     }
