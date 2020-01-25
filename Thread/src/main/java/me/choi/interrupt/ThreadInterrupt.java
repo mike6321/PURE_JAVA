eb3971402ca92e66008f0d696f9c7de250be2297
choijunwoo <mike6321@naver.com>
2020-01-25 16:07:21 +0900

    thread의 실행제어 (interrupt) 2

--- Thread/src/main/java/me/choi/interrupt/ThreadInterrupt.java
+++ Thread/src/main/java/me/choi/interrupt/ThreadInterrupt.java
@@ -6,7 +6,6 @@ public class ThreadInterrupt {
     public static void main(String[] args) {
         ThreadInterrupt_1 th1 = new ThreadInterrupt_1();
         th1.start();
-        System.out.println("isInterrupted() : "+ th1.isInterrupted());
 
         String input = JOptionPane.showInputDialog("아무 값이나 입력하세요");
         System.out.println("입력하신 값은 "+input+"입니다.");
@@ -21,14 +20,12 @@ class ThreadInterrupt_1 extends Thread{
 
         while (i != 0 && !isInterrupted()){
             System.out.println(i--);
-            for (long x =0 ;x<2500000000L;x++); //시간지연
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                interrupt();
+            }
         }
         System.out.println("카운트가 종료되었습니다.");
-    }
-}
-class ThreadInterrupt_2 extends Thread{
-    @Override
-    public void run() {
-
     }
 }
