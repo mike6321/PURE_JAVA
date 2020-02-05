3df3377b7957f36ab6d8c250390c9a47870b08f0
junwoo <33277588+mike6321@users.noreply.github.com>
2020-02-06 01:28:32 +0900

    string vs stringbuffer vs stringbuilder

--- EffectiveStudy/src/main/java/me/choi/stringEx/StringBuilderTest.java
+++ EffectiveStudy/src/main/java/me/choi/stringEx/StringBuilderTest.java
@@ -1,4 +1,10 @@
 package me.choi.stringEx;
 
 public class StringBuilderTest {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(2);
+        sb.append("12345");
+        sb.append("1");
+        sb.append("1");
+    }
 }
--- EffectiveStudy/src/main/java/me/choi/stringEx/StringTest.java
+++ EffectiveStudy/src/main/java/me/choi/stringEx/StringTest.java
@@ -1,4 +1,46 @@
 package me.choi.stringEx;
 
 public class StringTest {
+    final int cnt = 99999;
+
+    public static void main(String[] args) {
+        StringTest st = new StringTest();
+
+        //System.out.println("String :: "+st.StringTest("test"));
+        System.out.println("StringBuilder :: "+st.StringBuilderTest("testtest"));
+
+        //System.out.println("StringBuffer :: "+st.StringBufferTest(null));
+
+    }
+
+    public  long StringTest(String str) {
+        long start = System.currentTimeMillis();
+        for (int i=0 ; i<cnt ;i++) {
+            str += "test";
+        }
+        return System.currentTimeMillis() - start;
+    }
+
+    public  long StringBuilderTest(String str) {
+        StringBuilder sb = new StringBuilder(3);
+        System.out.println(sb.capacity());
+
+        long start = System.currentTimeMillis();
+        for (int i=0 ; i<cnt ;i++) {
+            sb.append("test");
+        }
+        System.out.println(sb.capacity());
+        return System.currentTimeMillis() - start;
+    }
+
+    public long StringBufferTest(String str) {
+        StringBuffer sb = new StringBuffer(str);
+
+        long start = System.currentTimeMillis();
+        for (int i=0 ; i<cnt ;i++) {
+            sb.append("test");
+        }
+        return System.currentTimeMillis() - start;
+    }
+
 }
