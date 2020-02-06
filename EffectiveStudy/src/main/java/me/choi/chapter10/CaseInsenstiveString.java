f1a4019627ea34ff119c2be9287112167747b57b
junwoo <33277588+mike6321@users.noreply.github.com>
2020-02-06 23:15:26 +0900

    equals 대칭성 위배(2)

--- EffectiveStudy/src/main/java/me/choi/chapter10/CaseInsenstiveString.java
+++ EffectiveStudy/src/main/java/me/choi/chapter10/CaseInsenstiveString.java
@@ -1,25 +1,38 @@
 package me.choi.chapter10;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class CaseInsenstiveString {
     private final String s;
 
-    public static void main(String[] args) {
-        CaseInsenstiveString cs = new CaseInsenstiveString("choi");
-        boolean result = cs.equals("cHoi");
 
+    public static void main(String[] args) {
+
+        // TODO: [main] junwoochoi 06/02/2020 11:04 오후
+        // 대칭성 위배 - String , List (1)
+       CaseInsenstiveString cs = new CaseInsenstiveString("choi");
+        boolean result = cs.equals("cHoi");
         System.out.println(result);
 
         String s = new String("cHoi");
         boolean result2 = s.equals(cs);
-
         System.out.println(result2);
+
+        List<CaseInsenstiveString> list = new ArrayList<>();
+        list.add(cs);
+
+        boolean result3 = list.contains(s);
+        System.out.println(result3);
+
+
     }
 
     public CaseInsenstiveString(String s) {
         Objects.requireNonNull(this.s = s);
     }
+
 
     public boolean equals(Object obj) {
         if (obj instanceof CaseInsenstiveString) {
