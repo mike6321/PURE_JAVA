082d16ce6d540a0072a8d329d3d9714ea191baf6
junwoo <33277588+mike6321@users.noreply.github.com>
2020-02-18 20:09:56 +0900

    테스트 스터디 준비 init

---
+++ demostudytesting/src/main/java/me/choi/demostudytesting/DemostudytestingApplication.java
@@ -0,0 +1,13 @@
+package me.choi.demostudytesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemostudytestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemostudytestingApplication.class, args);
+    }
+
+}
