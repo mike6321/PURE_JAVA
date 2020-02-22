7e3a6186a8b0aa009510ebd5af394714ebee1656
junwoo <33277588+mike6321@users.noreply.github.com>
2020-02-22 22:50:07 +0900

    버블정렬 1

---
+++ Algorithm/src/main/java/me/choi/sort/BubbleSort.java
@@ -0,0 +1,47 @@
+package me.choi.sort;
+
+import java.util.Scanner;
+
+/**
+ * Project : Algorithm
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/02/22
+ * Time : 9:43 오후
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("버블정렬");
+        System.out.println("요솟수 : ");
+        int nx = sc.nextInt();
+        int[] x = new int[nx];
+
+        for (int i=0 ;i<nx; i++) {
+            x[i] = sc.nextInt();
+        }
+        bubbleSort(x, nx);
+
+        for (int i=0 ;i<nx; i++) {
+            System.out.printf("x["+i+"]="+x[i]+ " ");
+        }
+
+
+    }
+
+    static void bubbleSort(int[] array, int length) {
+        for (int i=0;i<length-1;i++) {
+            for (int k=length-1 ; k>i; k--) {
+                if (array[k-1] > array[k])
+                    swap(array, k);
+            }
+        }
+    }
+
+    private static void swap(int[] array, int i) {
+        int temp = array[i];
+        array[i] = array[i-1];
+        array[i-1] = temp;
+    }
+}
