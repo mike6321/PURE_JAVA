a2805be6d6e52b50b68f6c51061e86ff91a61fab
junwoo <33277588+mike6321@users.noreply.github.com>
2020-02-22 21:11:42 +0900

    알고리즘 init

---
+++ EffectiveStudy/src/main/java/me/choi/others/finalex/StaticFinalExample.java
@@ -0,0 +1,11 @@
+package me.choi.others.finalex;
+
+/**
+ * Project : EffectiveStudy
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/02/21
+ * Time : 12:53 오전
+ */
+public class StaticFinalExample {
+}
