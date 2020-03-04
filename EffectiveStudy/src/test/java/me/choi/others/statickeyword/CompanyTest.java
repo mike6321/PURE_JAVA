a6a1e476da6cb866c1a18296b9edd311dd5ebea1
junwoo <33277588+mike6321@users.noreply.github.com>
2020-03-04 21:58:54 +0900

    jacoco 추가

---
+++ EffectiveStudy/src/test/java/me/choi/others/statickeyword/CompanyTest.java
@@ -0,0 +1,21 @@
+package me.choi.others.statickeyword;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Project : EffectiveStudy
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/03/04
+ * Time : 9:38 오후
+ */
+public class CompanyTest {
+    @Test
+    public void when() {
+        Company company0 = new Company("choi",01234);
+        Company company1 = new Company("kim",01235);
+        
+    }
+}
