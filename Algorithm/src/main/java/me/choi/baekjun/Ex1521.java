15d098604c349fbca04d247d7de4679d552757dc
junwoo <33277588+mike6321@users.noreply.github.com>
2020-03-09 21:36:11 +0900

    백준 2920 음계

---
+++ Algorithm/src/main/java/me/choi/baekjun/Ex1521.java
@@ -0,0 +1,66 @@
+package me.choi.baekjun;
+
+import java.util.Scanner;
+
+/**
+ * Project : Algorithm
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/03/09
+ * Time : 9:08 오후
+ */
+public class Ex1521 {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[8];
+        Scanner sc = new Scanner(System.in);
+        String result = "mixed";
+
+        for(int i=0 ; i<8  ; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        if (Asending(array)) {
+            result = "ascending";
+        }
+        if (Decending(array)) {
+            result = "descending";
+        }
+
+        System.out.println(result);
+    }
+
+    static boolean Asending(int[] array) {
+
+        boolean result = false;
+
+        for (int i=0 ; i<7 ; i++) {
+            if (array[i+1] == array[i]+1) {
+                result = true;
+            }else {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    static boolean Decending(int[] array) {
+
+        boolean result = false;
+
+        for (int i=0 ; i<7 ; i++) {
+            if (array[i+1] == array[i]-1) {
+                result = true;
+            }else {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
+
