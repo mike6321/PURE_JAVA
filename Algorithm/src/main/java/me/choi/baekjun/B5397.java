34dfa789950943bebf6eb546118098df950bb10c
junwoo <33277588+mike6321@users.noreply.github.com>
2020-03-13 21:29:42 +0900

    백준 5397 키로거(ing)

---
+++ Algorithm/src/main/java/me/choi/baekjun/B5397.java
@@ -0,0 +1,25 @@
+package me.choi.baekjun;
+
+import java.util.Scanner;
+
+/**
+ * Project : Algorithm
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/03/13
+ * Time : 9:04 오후
+ */
+public class B5397 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int L = sc.nextInt();
+
+        for (int i=0 ; i<L ; i++) {
+            StringBuffer sb = new StringBuffer();
+            sb.append(sc.next());
+
+
+        }
+
+    }
+}
