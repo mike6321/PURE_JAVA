88e9a01360c31bd43b25bececdee354628fb5880
junwoo <33277588+mike6321@users.noreply.github.com>
2020-04-20 01:24:00 +0900

    전략패턴 commit

---
+++ EffectiveStudy/src/main/java/me/designpattern/strategypattern/example/Knife.java
@@ -0,0 +1,15 @@
+package me.designpattern.strategypattern.example;
+
+/**
+ * Project : croissant
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/04/20
+ * Time : 1:09 오전
+ */
+public class Knife implements Weapon{
+    @Override
+    public void attack() {
+        System.out.println("Knife Attack");
+    }
+}
