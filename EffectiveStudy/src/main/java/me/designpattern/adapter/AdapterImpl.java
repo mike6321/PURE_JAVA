68904741fa507dd5a7e4944b5da654592434b2e1
junwoo <33277588+mike6321@users.noreply.github.com>
2020-04-21 22:27:36 +0900

    어댑터패턴 commit

---
+++ EffectiveStudy/src/main/java/me/designpattern/adapter/AdapterImpl.java
@@ -0,0 +1,25 @@
+package me.designpattern.adapter;
+
+/**
+ * Project : EffectiveStudy
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/04/21
+ * Time : 9:52 오후
+ */
+public class AdapterImpl implements Adapter{
+
+    @Override
+    public float twiceOf(Float f) {
+
+//        return (float) Math.twoTime(f.doubleValue());
+        return Math.doubled(f.doubleValue()).floatValue();
+    }
+
+    @Override
+    public float halfOf(Float f) {
+        System.out.println("[Log] 절반 함수 호출");
+
+        return (float) Math.half(f.doubleValue());
+    }
+}
