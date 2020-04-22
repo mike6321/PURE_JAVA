62c94441721d6fc462dd33e8013616029d46d809
junwoo <33277588+mike6321@users.noreply.github.com>
2020-04-22 23:00:09 +0900

    템플릿메서드패턴 commit

---
+++ EffectiveStudy/src/main/java/me/designpattern/factorymethod/concrete/HpPotion.java
@@ -0,0 +1,17 @@
+package me.designpattern.factorymethod.concrete;
+
+import me.designpattern.factorymethod.framework.Item;
+
+/**
+ * Project : EffectiveStudy
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/04/22
+ * Time : 10:45 오후
+ */
+public class HpPotion implements Item {
+    @Override
+    public void use() {
+        System.out.println("체력 회복!");
+    }
+}
---
+++ EffectiveStudy/src/main/java/me/designpattern/factorymethod/framework/Item.java
@@ -0,0 +1,12 @@
+package me.designpattern.factorymethod.framework;
+
+/**
+ * Project : EffectiveStudy
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/04/22
+ * Time : 10:31 오후
+ */
+public interface Item {
+    public void use();
+}
