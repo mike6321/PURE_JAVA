d5dcee06e237358d179ff56c61c0e4cffdcc4516
junwoo <33277588+mike6321@users.noreply.github.com>
2020-05-16 16:47:40 +0900

    추상팩토리메서드 패턴 main 수정

--- EffectiveStudy/src/main/java/me/designpattern/h_abstractfactory/step01/Main.java
+++ EffectiveStudy/src/main/java/me/designpattern/h_abstractfactory/step01/Main.java
@@ -1,10 +1,7 @@
 package me.designpattern.h_abstractfactory.step01;
 
 import me.designpattern.h_abstractfactory.step01.abst.BikeFactory;
-import me.designpattern.h_abstractfactory.step01.abst.Body;
-import me.designpattern.h_abstractfactory.step01.abst.Wheel;
 import me.designpattern.h_abstractfactory.step01.gt.GtBikeFactory;
-import me.designpattern.h_abstractfactory.step01.sam.SamFactory;
 
 /**
  * Project : EffectiveStudy
@@ -15,19 +12,9 @@ import me.designpattern.h_abstractfactory.step01.sam.SamFactory;
  */
 public class Main {
     public static void main(String[] args) {
-        BikeFactory factory = new SamFactory();
+        BikeFactory factory = new GtBikeFactory();
 
-        Body body = factory.createBody();
-        Wheel wheel = factory.createWheel();
-
-        System.out.println(body.getClass());
-        System.out.println(wheel.getClass());
-
-        factory = new GtBikeFactory();
-        Body body1 = factory.createBody();
-        Wheel wheel1 = factory.createWheel();
-
-        System.out.println(body1.getClass());
-        System.out.println(wheel1.getClass());
+        System.out.println(factory.createBody().getClass());
+        System.out.println(factory.createWheel().getClass());
     }
 }
