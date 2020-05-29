4a3742818956f9b86cb60c4f2c8a8b6fc4edfacc
junwoo <33277588+mike6321@users.noreply.github.com>
2020-05-29 21:51:17 +0900

    컴포짓 패턴

---
+++ EffectiveStudy/src/main/java/me/designpattern/j_composit/File.java
@@ -0,0 +1,11 @@
+package me.designpattern.j_composit;
+
+/**
+ * Project : EffectiveStudy
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 9:06 오후
+ */
+public class File {
+}
---
+++ EffectiveStudy/src/main/java/me/designpattern/j_composit/Folder.java
@@ -0,0 +1,11 @@
+package me.designpattern.j_composit;
+
+/**
+ * Project : EffectiveStudy
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 9:07 오후
+ */
+public class Folder {
+}
