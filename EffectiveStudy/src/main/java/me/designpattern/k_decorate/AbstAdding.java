d375801cfa72cc5d41524e8064ae3834e4740500
junwoo <33277588+mike6321@users.noreply.github.com>
2020-05-30 21:27:51 +0900

    데코레이터 패턴

---
+++ EffectiveStudy/src/main/java/me/designpattern/k_decorate/AbstAdding.java
@@ -0,0 +1,11 @@
+package me.designpattern.k_decorate;
+
+/**
+ * Project : EffectiveStudy
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 9:25 오후
+ */
+public class AbstAdding {
+}
---
+++ EffectiveStudy/src/main/java/me/designpattern/k_decorate/IBeverage.java
@@ -0,0 +1,11 @@
+package me.designpattern.k_decorate;
+
+/**
+ * Project : EffectiveStudy
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 9:24 오후
+ */
+public class IBeverage {
+}
