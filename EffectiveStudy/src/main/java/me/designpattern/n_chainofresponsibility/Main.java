b4fe271ea99865c4eb74ad7e6f513a0cb9234669
junwoo <33277588+mike6321@users.noreply.github.com>
2020-05-30 21:34:01 +0900

    책임사슬 패턴(1)

---
+++ EffectiveStudy/src/main/java/me/designpattern/n_chainofresponsibility/Main.java
@@ -0,0 +1,11 @@
+package me.designpattern.n_chainofresponsibility;
+
+/**
+ * Project : EffectiveStudy
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 9:33 오후
+ */
+public class Main {
+}
---
+++ EffectiveStudy/src/main/java/me/designpattern/n_chainofresponsibility/Request.java
@@ -0,0 +1,11 @@
+package me.designpattern.n_chainofresponsibility;
+
+/**
+ * Project : EffectiveStudy
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 9:32 오후
+ */
+public class Request {
+}
