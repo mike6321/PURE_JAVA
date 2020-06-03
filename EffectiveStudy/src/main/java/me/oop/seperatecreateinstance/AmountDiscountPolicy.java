8b851a5c6ef32ffedd6ce41beb09f38325807168
junwoo <33277588+mike6321@users.noreply.github.com>
2020-06-03 22:17:56 +0900

    생성과 사용을 분리(1)

---
+++ EffectiveStudy/src/main/java/me/oop/seperatecreateinstance/AmountDiscountPolicy.java
@@ -0,0 +1,11 @@
+package me.oop.seperatecreateinstance;
+
+/**
+ * Project : EffectiveStudy
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 10:12 오후
+ */
+public class AmountDiscountPolicy {
+}
---
+++ EffectiveStudy/src/main/java/me/oop/seperatecreateinstance/Movie.java
@@ -0,0 +1,11 @@
+package me.oop.seperatecreateinstance;
+
+/**
+ * Project : EffectiveStudy
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 10:11 오후
+ */
+public class Movie {
+}
