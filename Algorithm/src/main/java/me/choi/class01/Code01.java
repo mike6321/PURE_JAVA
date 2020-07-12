a4c59d8b99642bc4177c44fb254208fca8cd68b2
choijunwoo <mike6321@naver.com>
2020-07-12 17:10:20 +0900

    Algo first init

---
+++ Algorithm/src/main/java/me/choi/class01/Code01.java
@@ -0,0 +1,14 @@
+package me.choi.class01;
+
+public class Code01 {
+    public static void main(String[] args) {
+        func();
+    }
+
+    private static void func() {
+        System.out.println("hello world!");
+        func();
+    }
+
+
+}
