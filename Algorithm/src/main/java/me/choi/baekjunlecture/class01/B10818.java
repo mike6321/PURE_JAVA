4c8fde1f683c98f21aa35b2c06668e0f0892a2ad
junwoo <33277588+mike6321@users.noreply.github.com>
2020-07-15 22:50:34 +0900

    Baekjun clas01 (2) - Arithmetic

---
+++ Algorithm/src/main/java/me/choi/baekjunlecture/class01/B10818.java
@@ -0,0 +1,50 @@
+package me.choi.baekjunlecture.class01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * Project : Algorithm
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 9:42 오후
+ */
+public class B10818 {
+    public static void main(String[] args) throws IOException {
+        int N = 0;
+        int[] arr;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        String line = br.readLine();
+        st = new StringTokenizer(line, " ");
+        br.close();
+
+        for (int i=0 ; i<N ;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i=0 ; i<N ; i++) {
+            if (min > arr[i])
+                min = arr[i];
+
+            if (max < arr[i])
+                max = arr[i];
+        }
+
+        System.out.print(min+ " "+max);
+
+
+    }
+}
---
+++ Algorithm/src/main/java/me/choi/baekjunlecture/class01/B8393.java
@@ -0,0 +1,26 @@
+package me.choi.baekjunlecture.class01;
+
+import java.util.Scanner;
+
+/**
+ * Project : Algorithm
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 9:38 오후
+ */
+public class B8393 {
+    public static void main(String[] args) {
+        int n = 0;
+        int sum = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+
+        for (int i = 1;i <= n ;i++) {
+                sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
