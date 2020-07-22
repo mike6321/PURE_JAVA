10772ee17f604f5eefb1339cdecf43389df29551
junwoo <33277588+mike6321@users.noreply.github.com>
2020-07-23 00:31:58 +0900

    lamda expression 정리 완료

--- java8to11/src/main/java/me/choi/java8to11/Foo.java
+++ java8to11/src/main/java/me/choi/java8to11/Foo.java
@@ -12,71 +12,40 @@ import java.util.function.*;
 public class Foo {
 
     public static void main(String[] args) {
+        Foo foo = new Foo();
+        foo.run();
+    }
 
-        // 익명 내부 클래스
-        RunSomething runSomething1 = new RunSomething() {
+    public void run() {
+
+        int baseNumber = 10;
+
+        /*로컬 클래스*/
+        class LocalClass {
+            void printBaseNumber() {
+                int baseNumber = 11;
+                System.out.println("Local Class : "+baseNumber);
+            }
+        }
+
+        /*익명 클래스*/
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
+            int baseNumber = 11;
             @Override
-            public int doIt(int number) {
-                return 0;
+            public void accept(Integer integer) {
+                System.out.println("Anonymous Class : "+baseNumber);
             }
         };
 
-
-//        RunSomething runSomething2 = () -> {
-//            System.out.println("Hello!");
-//            System.out.println("Lamda");
-//        };
-
-        RunSomething runSomething3 = (number) -> {
-            return number+10;
+        /*람다*/
+        IntConsumer printint = (i) -> {
+            System.out.println("Lamda : "+ (i+ baseNumber));
         };
 
 
-//        System.out.println(runSomething3.doIt(5));
-//        System.out.println(runSomething3.doIt(5));
-//        System.out.println(runSomething3.doIt(5));
-//        System.out.println(runSomething3.doIt(5));
+        printint.accept(10);
 
-        Plus10 plus10 = new Plus10();
-        System.out.println(plus10.apply(10));
-
-        System.out.println();
-        System.out.println("Function");
-
-        Function<Integer, Integer> plus5 = (i) -> i+5;
-        Function<Integer, Integer> multiply2 = (i) -> i*2;
-
-        Function<Integer, Integer> multiply2AndPlus5 = plus5.compose(multiply2);
-        System.out.println(multiply2AndPlus5.apply(2));
-
-        Function<Integer, Integer> plus5AndMultiply2 = plus5.andThen(multiply2);
-        System.out.println(plus5AndMultiply2.apply(5));
-
-
-        System.out.println();
-        System.out.println("Consumer");
-
-        Consumer<Integer> printT = (i) -> System.out.println(i);
-        printT.accept(3);
-
-
-        System.out.println();
-        System.out.println("Supplier");
-
-        Supplier<Integer> get10 = () -> 10;
-        System.out.println(get10.get());
-
-
-        System.out.println();
-        System.out.println("Predicate");
-        Predicate<String> startsWithJunwoo = (s) -> s.startsWith("junwoo");
-        Predicate<Integer> isEven = (i) -> i%2 == 0;
-
-        System.out.println(startsWithJunwoo.test("djflkjflkdf"));;
-        System.out.println(startsWithJunwoo.test("junwoo"));
-
-        UnaryOperator<Integer> plus11 = (i) -> i+11;
-
-
+        LocalClass localClass = new LocalClass();
+        localClass.printBaseNumber();
     }
 }
