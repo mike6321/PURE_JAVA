c7af349a482f8caf0d0f84af8eb3bd407d011cfd
junwoo <33277588+mike6321@users.noreply.github.com>
2020-07-26 23:47:28 +0900

    메멘토 패턴

---
+++ EffectiveStudy/src/main/java/me/designpattern/r_memento/Application.java
@@ -0,0 +1,11 @@
+package me.designpattern.r_memento;
+
+/**
+ * Project : EffectiveStudy
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 10:47 오후
+ */
+public class Application {
+}
---
+++ EffectiveStudy/src/main/java/me/designpattern/r_memento/abc/Memento.java
@@ -0,0 +1,11 @@
+package me.designpattern.r_memento.abc;
+
+/**
+ * Project : EffectiveStudy
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 11:11 오후
+ */
+public class Memento {
+}
