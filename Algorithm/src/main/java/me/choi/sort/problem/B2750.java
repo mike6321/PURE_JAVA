c56b6adf34ff84186ddaacfe178a6520d4c5c8be
junwoo <33277588+mike6321@users.noreply.github.com>
2020-08-17 00:15:09 +0900

    fix (B2750 선택정렬 풀이)

--- Algorithm/src/main/java/me/choi/sort/problem/B2750.java
+++ Algorithm/src/main/java/me/choi/sort/problem/B2750.java
@@ -7,28 +7,45 @@ import java.util.Scanner;
  * Project : Algorithm
  *
  * @author : jwdeveloper
- * @comment :
+ * @comment : 시간복잡도가 O(n^2)을 해도 1억이 안되기 떄문에 단순 선택정렬을 선택해도 무리가 없다.
  * Time : 11:46 오후
  */
 public class B2750 {
-    private static int MAX = Integer.MIN_VALUE;
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
         int[] arr = new int[N];
-        int[] result = new int[N];
+        int min = 0;
+        int temp = 0;
+        int index = 0;
 
         for (int i = 0; i < N; i++) {
             arr[i] = scanner.nextInt();
         }
 
-        Arrays.sort(arr);
+        for (int i = 0; i < N; i++) {
+            min = 1001;
+            for (int j = i; j < N; j++) {
+                if (min > arr[j]) {
+                    min = arr[j];
+                    index = j;
+                }
+            }
+            swap(arr, index, i);
+        }
 
         for (int i = 0; i < N; i++) {
             System.out.println(arr[i]);
         }
 
+    }
+
+    private static void swap(int[] arr, int index, int i) {
+        int temp;
+        temp = arr[i];
+        arr[i] = arr[index];
+        arr[index] = temp;
     }
 }
