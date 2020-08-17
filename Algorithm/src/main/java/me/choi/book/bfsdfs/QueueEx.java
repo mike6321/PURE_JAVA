19a883ab1a38abbc59da4ac69a6673d1193f3d24
junwoo <33277588+mike6321@users.noreply.github.com>
2020-08-17 22:35:55 +0900

    fix (Queue 내용 정리)

--- Algorithm/src/main/java/me/choi/book/bfsdfs/QueueEx.java
+++ Algorithm/src/main/java/me/choi/book/bfsdfs/QueueEx.java
@@ -1,8 +1,34 @@
-package me.choi.book.bfsdfs;/**
+package me.choi.book.bfsdfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
  * Project : Algorithm
- * 
+ *
  * @author : jwdeveloper
- * @comment : 
+ * @comment :
  * Time : 10:27 오후
-*/public class QueueEx {
+ */
+public class QueueEx {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(3);
+        queue.offer(4);
+        queue.poll();
+        queue.offer(5);
+        queue.offer(7);
+
+        /*
+        * 3 4
+        * 4
+        * 4 5 7
+        * */
+
+        while (!queue.isEmpty()) {
+            System.out.println(queue.peek());
+            queue.poll();
+        }
+    }
 }
