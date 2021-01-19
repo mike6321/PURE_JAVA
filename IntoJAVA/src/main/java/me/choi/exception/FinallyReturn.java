a9dbf14b6aab08701757903c31b7c500e702d35c
junwoo <33277588+mike6321@users.noreply.github.com>
2021-01-19 16:31:47 +0900

    Finally block Return - comment 추가

--- IntoJAVA/src/main/java/me/choi/exception/FinallyReturn.java
+++ IntoJAVA/src/main/java/me/choi/exception/FinallyReturn.java
@@ -14,13 +14,18 @@ public class FinallyReturn {
         System.out.println(print);
     }
     public StringBuilder print() {
+        // TODO: [Finally Return] junwoochoi 2021/01/19 4:29 오후
+        /**
+         * finally block에 리턴이 있으면 try block에 있는 return 을 수행하지 않는다.
+         * 안티패턴이므로 사용하지 말자!
+         * */
         StringBuilder sb = new StringBuilder();
         try {
             sb.append("Hi");
             sb.append(" My name is ");
-            String name = "123";
+            String name = "junwoo";
             sb.append(name);
-
+            //throw new IllegalArgumentException();
             return sb;
         } catch (Exception e) {
             sb.append(" catch pass");
