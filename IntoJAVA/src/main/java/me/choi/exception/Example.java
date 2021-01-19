54a9ad1b89008fb08ae7f4ead0f97a913ec7742e
junwoo <33277588+mike6321@users.noreply.github.com>
2021-01-19 13:17:20 +0900

    (예외) 하위 클래스를 하위 catch로 선언하지마라

---
+++ IntoJAVA/src/main/java/me/choi/exception/Example.java
@@ -0,0 +1,25 @@
+package me.choi.exception;
+
+/**
+ * Project : IntoJAVA
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 12:48 오후
+ */
+public class Example {
+    public static void main(String[] args) {
+        try {
+
+        } catch (RuntimeException e) {
+
+        }
+        // TODO: [IllegalArgumentException] junwoochoi 2021/01/19 1:02 오후 
+        /**
+         * 
+         * */
+//        } catch (IllegalArgumentException e) {
+//
+//        }
+    }
+}
