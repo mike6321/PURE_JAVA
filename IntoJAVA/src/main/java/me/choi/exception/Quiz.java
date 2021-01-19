69f9bbc929a964b37a0d5fcb01b58235848183d2
junwoo <33277588+mike6321@users.noreply.github.com>
2021-01-19 14:49:14 +0900

    Quiz (문제점 코딩)

--- IntoJAVA/src/main/java/me/choi/exception/Quiz.java
+++ IntoJAVA/src/main/java/me/choi/exception/Quiz.java
@@ -14,27 +14,30 @@ public class Quiz {
     static void copy (String src, String dest) throws IOException {
         InputStream in = null;
         OutputStream out = null;
-
         try {
-            in = new FileInputStream(src);
             out = new FileOutputStream(dest);
-            byte[] buf = new byte[1024];
-            int n;
-            while ((n = in.read(buf)) >= 0) {
-                out.write(buf, 0, n);
-            }
-        } finally {
-            // TODO: [만약 IOException이아니라 RuntimeException 이라면?] junwoochoi 2021/01/19 2:44 오후
-            /**
-             * RuntimeException 이라면 뒤에 close 예외를 처리하지 않는다.
-             * */
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
 
+            try {
+                in = new FileInputStream(src);
+                byte[] buf = new byte[1024];
+                int n;
+                while ((n = in.read(buf)) >= 0) {
+                    out.write(buf, 0, n);
+                }
+            } finally {
+                // TODO: [만약 IOException이아니라 RuntimeException 이라면?] junwoochoi 2021/01/19 2:44 오후
+                /**
+                 * RuntimeException 이라면 뒤에 close 예외를 처리하지 않는다.
+                 * */
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+
+                    }
                 }
             }
+        } finally {
             if (out != null) {
                 try {
                     out.close();
