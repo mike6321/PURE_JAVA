6e98ebbfb279f2b0866a2a43a7952f9122cb3b57
junwoo <33277588+mike6321@users.noreply.github.com>
2021-01-19 14:44:50 +0900

    Quiz (문제점 파악)

--- IntoJAVA/src/main/java/me/choi/exception/Quiz.java
+++ IntoJAVA/src/main/java/me/choi/exception/Quiz.java
@@ -24,6 +24,10 @@ public class Quiz {
                 out.write(buf, 0, n);
             }
         } finally {
+            // TODO: [close에 문제가 있다.] junwoochoi 2021/01/19 2:44 오후
+            /**
+             * 첫 번째 close가 비정상 종료 되었을때 다음 close를 타지 않는다.
+             * */
             if (in != null) {
                 in.close();
             }
