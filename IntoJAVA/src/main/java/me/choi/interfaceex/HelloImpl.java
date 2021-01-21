f904aa5f8939bd39c49edf87b92a827570bf789c
junwoo <33277588+mike6321@users.noreply.github.com>
2021-01-21 14:40:30 +0900

    (Interface) 어댑터클래스

---
+++ IntoJAVA/src/main/java/me/choi/interfaceex/HelloImpl.java
@@ -0,0 +1,8 @@
+package me.choi.interfaceex;/**
+ * Project : IntoJAVA
+ * 
+ * @author : jwdeveloper
+ * @comment : 
+ * Time : 2:35 오후
+*/public class HelloImpl {
+}
---
+++ IntoJAVA/src/main/java/me/choi/interfaceex/HelloInterface.java
@@ -0,0 +1,8 @@
+package me.choi.interfaceex;/**
+ * Project : IntoJAVA
+ * 
+ * @author : jwdeveloper
+ * @comment : 
+ * Time : 2:34 오후
+*/public interface HelloInterface {
+}
