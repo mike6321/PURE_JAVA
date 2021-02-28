4827d33aace125914d332d31c4e0e15221170a5e
junwoo <33277588+mike6321@users.noreply.github.com>
2021-02-28 22:02:20 +0900

    Refactoring complete

--- IntoJAVA/src/main/java/me/choi/generic/generic2/Store.java
+++ IntoJAVA/src/main/java/me/choi/generic/generic2/Store.java
@@ -11,7 +11,13 @@ import java.util.List;
  */
 public class Store {
     public static void main(String[] args) {
-        AppleDao appleDao = new AppleDao();
+//        AppleDao appleDao = new AppleDao();
+        // TODO: 제네릭 레펙토링 2021/02/28 9:55 오후
+        /**
+         * 이렇게 하면 Banana, Apple이 필요 없어진다.
+         * */
+        GenericDao<Apple, Integer> appleDao = new GenericDao<>();
+
         appleDao.save(Apple.of(1));
         appleDao.save(Apple.of(2));
         appleDao.save(Apple.of(3));
