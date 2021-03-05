eb8dc3173d4ff956bbd053cf2a2f588df4e7bda6
junwoo <33277588+mike6321@users.noreply.github.com>
2021-03-05 12:53:29 +0900

    chore (add comment)

--- IntoJAVA/src/test/java/me/choi/optional/example03/OptionalTest.java
+++ IntoJAVA/src/test/java/me/choi/optional/example03/OptionalTest.java
@@ -1,7 +1,9 @@
 package me.choi.optional.example03;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,8 +23,15 @@ public class OptionalTest {
         springClass.add(new OnlineClass(2, "spring data jpa", true));
         springClass.add(new OnlineClass(3, "spring mvc", false));
         springClass.add(new OnlineClass(4, "spring core", false));
-        springClass.add(new OnlineClass(5, "rest api", true));
+        springClass.add(new OnlineClass(5, "rest api", false));
 
+        // TODO: Optional 개요 2021/03/05 12:50 오후
+        /**
+         * 1. Null을 리턴하는 것 자체가 문제
+         * 2. if 로 예외를 처리하면 사람이기떄문에 분명 실수를 한다.
+         * */
+        OnlineClass spring_boot = new OnlineClass(1, "spring boot", true);
+        Assertions.assertThat(spring_boot.getProgress()).isNull();
 
 
     }
